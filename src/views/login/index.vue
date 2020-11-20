ef3834ebe78a9586ac09277d8bf9b8833f83eec8
lzread <lzread@163.com>
2020-11-20 10:14:07 +0800

    test jenkins

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -42,7 +42,7 @@
         <button @click="login">{{ $t("Login.buttonText") }}</button>
 
         <p><a href="#">MIT</a> license</p>
-        <p>Copyright &copy; 2019-2022 vue.lzread.com </p>
+        <p>Copyright &copy; 2019-2021 vue.lzread.com </p>
 
       </div>
 
