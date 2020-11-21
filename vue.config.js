9eb5293832d21ad816c3efc3727acfb235e54fc1
lzread <lzread@163.com>
2020-11-21 16:53:45 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,17 @@ module.exports = {
       warnings: false,
       errors: true
     },
+    proxy: {//配置跨域
+      '/api': {
+        target: 'http://api.lzread.com/',//这里后台的地址模拟的;应该填写你们真实的后台接口
+        ws: true,
+        changOrigin: true,//允许跨域
+        pathRewrite: {
+          '^/api': ''//请求的时候使用这个api就可以
+        }
+      }
+
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
